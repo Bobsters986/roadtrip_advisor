08f8957618dd0c86b9958f4167631c4a98f48d07
Bobby Luly <bobbyluly@gmail.com>
2023-04-23 20:10:53 -0600

    Test: write password security confirmation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,16 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:password) }
     it { should validate_confirmation_of(:password) }
+    it { should have_secure_password }
+  end
+
+  describe 'Password encryption' do
+    it 'encrypts the password using BCrypt' do
+      user = User.create(email: 'meg@test.com', password: 'password123', password_confirmation: 'password123')
+        expect(user).to_not have_attribute(:password)
+        expect(user).to_not have_attribute(:password_confirmation)
+        expect(user.password_digest).to_not be_nil
+        expect(user.password_digest).to_not eq('password123')
+    end
   end
 end
