c8f28836bde456f54ada821a21cda0141e829abf
Bobby Luly <bobbyluly@gmail.com>
2023-04-24 09:31:58 -0600

    Pass teleport service spec tests and create vcr

--- app/services/teleport_service.rb
+++ app/services/teleport_service.rb
@@ -1,7 +1,7 @@
 class TeleportService
 
-  def self.get_coordinates(location)
-    response = conn.get("urban_areas/slug:#{location}/salaries/")
+  def self.get_salaries(destination)
+    response = conn.get("urban_areas/slug:#{destination}/salaries/")
     JSON.parse(response.body, symbolize_names: true)
   end
 
--- spec/services/teleport_service_spec.rb
+++ spec/services/teleport_service_spec.rb
@@ -1,4 +1,26 @@
 require 'rails_helper'
 
 describe TeleportService do
+  describe "#get_salaries" do
+    it "returns salaries for a location, Chicago" do
+      VCR.use_cassette("teleport_chicago_salaries") do
+        destination = "chicago"
+        response = TeleportService.get_salaries(destination)
+
+        expect(response).to be_a(Hash)
+        expect(response[:salaries]).to be_an(Array)
+        expect(response[:salaries][0].keys).to eq([:job, :salary_percentiles])
+
+        expect(response[:salaries][0][:job]).to be_a(Hash)
+        expect(response[:salaries][0][:job].keys).to eq([:id, :title])
+        expect(response[:salaries][0][:job][:id]).to eq("ACCOUNT-MANAGER")
+        expect(response[:salaries][0][:job][:title]).to eq("Account Manager")
+
+        expect(response[:salaries][0][:salary_percentiles]).to be_a(Hash)
+        expect(response[:salaries][0][:salary_percentiles].keys).to eq([:percentile_25, :percentile_50, :percentile_75])
+        expect(response[:salaries][0][:salary_percentiles][:percentile_25]).to eq(53732.4997858553)
+        expect(response[:salaries][0][:salary_percentiles][:percentile_75]).to eq(84227.57438186172)
+      end
+    end
+  end
 end
