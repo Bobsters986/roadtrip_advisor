c1ccecf2f98ea9f573789e813eabbddf14f81c6d
Bobby Luly <bobbyluly@gmail.com>
2023-04-23 21:49:18 -0600

    Add post /api/v1/users to users#create

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get '/forecast', to: 'forecast#index'
+      resources :forecast, only: [:index]
+      resources :users, only: [:create]
     end
   end
 end
