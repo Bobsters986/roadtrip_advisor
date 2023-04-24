df5149df05284a43a844257f20af0ffb43ba2c4e
Bobby Luly <bobbyluly@gmail.com>
2023-04-23 21:17:43 -0600

    Test&Feat: create tests and callback for creating a random api key for a user before they are created

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   validates :password, presence: true, confirmation: true
 
   has_secure_password
+
+  before_create :create_api_key
+
+  private
+
+  def create_api_key
+    self.api_key = SecureRandom.hex(13)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,4 +18,16 @@ RSpec.describe User, type: :model do
         expect(user.password_digest).to_not eq('password123')
     end
   end
+
+  describe 'instance methods' do
+    describe '#create_api_key' do
+      it 'creates an api key for the user in a callback' do
+        user = User.create(email: 'bob@test.com', password: 'password123', password_confirmation: 'password123')
+
+        expect(user.api_key).to_not be_nil
+        expect(user.api_key).to be_a(String)
+        expect(user.api_key.length).to eq(26)
+      end
+    end
+  end
 end
