04adc1289a39f4e3a95a8a00a84e1d0919d8dca8
Bobby Luly <bobbyluly@gmail.com>
2023-04-25 18:23:15 -0600

    Feat: create #get_arrival_time to pass tests. #create_weather_hash and #new_adventure in progress

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -15,7 +15,16 @@ class RoadTripFacade
     else
       travel_time = road_trip_data[:route][:formattedTime]
 
+      trip_start_time = forecast_at_destination[:location][:localtime]
+      trip_arrival_time = get_arrival_time(travel_time)
 
+      arrival_day_index = (trip_arrival_time.to_date - trip_start_time.to_date).to_i
+      exact_hour = trip_arrival_time.strftime("%H").to_i
+
+      arrival_weather = forecast_at_destination[:forecast][:forecastday][arrival_day_index][:hour][exact_hour]
+
+      weather_at_eta = create_weather_hash(arrival_weather)
+        
       RoadTrip.new(origin, destination, travel_time, weather_at_eta)
     end
   end
@@ -29,8 +38,24 @@ class RoadTripFacade
 
   def days_of_travel(travel_time)
     time = travel_time.split(":")
-    hours = time[0].to_i
-    days = (hours / 24).to_i
+    trip_hours = time[0].to_i
+    trip_days = (trip_hours / 24).to_i
+  end
+
+  def get_arrival_time(travel_time)
+    time = travel_time.split(":")
+    trip_hours = time[0].to_i
+    trip_minutes = time[1].to_i
+
+    arrival_time = Time.now + trip_hours.hours + trip_minutes.minutes
+  end
+
+  def create_weather_hash(arrival_weather)
+    {
+      datetime: arrival_weather[:time],
+      temperature: arrival_weather[:temp_f],
+      conditions: arrival_weather[:condition][:text]
+    }
   end
 
   private
@@ -38,7 +63,4 @@ class RoadTripFacade
   def road_trip_data
     @road_trip_data ||= MapquestService.get_travel_info(origin, destination)
   end
-
 end
-# weather_at_eta = WeatherFacade.new(road_trip_data[:route][:locations].last[:latLng][:lat], road_trip_data[:route][:locations].last[:latLng][:lng]).weather_at_eta(travel_time)
-# weather_at_eta = WeatherService.forecast(road_trip_data[:route][:locations][1][:latLng], road_trip_data[:route][:locations][1][:latLng])
